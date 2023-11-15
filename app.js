21d2b51e7c39897abc8e75b9e8999aafe3eacee7
İrem Sultan Örün <iremsultanorun2@gmail.com>
2023-11-15 19:32:10 +0300

    change

--- app.js
+++ app.js
@@ -1,50 +1,43 @@
-let container = document.querySelector('.container');
 
-function createShapes(parentElement, shape, count, attribute, className) {
-    for (let i = 0; i < count; i++) {
-        let element = document.createElement(shape);
-        if (shape === 'img') {
-            element.src = attribute;
-        }
-        element.classList.add(className);
-        parentElement.appendChild(element);
+const pageOne = document.querySelector(".pageOne");
 
-        element.style.top = Math.random() * -window.innerHeight + 'px';
-        element.style.left = Math.random() * window.innerWidth + 'px';
-    }
-
-    setTimeout(() => {
-        const snowflakeTypes = document.querySelectorAll('.' + className);
-        snowflakeTypes.forEach(snowflakeType => {
-            snowflakeType.style.transition = 'top 20s ease-out';
-            snowflakeType.style.top = window.innerHeight + 'px';
-        });
-
-        setTimeout(() => {
-            setTimeout(() => {
-                snowflakeTypes.forEach(snowflakeType => {
-                    snowflakeType.remove();
-                });
-            }, 20000)
-
-            createShapes(parentElement, shape, count, attribute, className);
-        }, 1000);
-    }, 1);
-}
-
-createShapes(container, 'img', 10, 'snowflake.png', 'snowflake');
-createShapes(container, 'div', 15, 'circleSnowflake', 'circleSnowflake');
+const sliders=document.querySelectorAll('.page')
+let discoverButton = document.querySelector('.exploreButton');
+document.addEventListener("DOMContentLoaded", function () {
 
 
+  // for (let i = 0; i < 40; i++) {
+  //   const snowflake = document.createElement("img");
+  //   snowflake.className = "snowflake";
+  //   snowflake.setAttribute("src", "snowflake.png");
+
+
+  //   snowflake.style.left = Math.random() * 100 + "vw";
+  //   snowflake.style.animationDuration = Math.random() * 3 + 2 + "s";
+
+  //   pageOne.appendChild(snowflake);
+  // }
+
+});
 let rightDiv = document.querySelector('.rightDiv')
-for (let i = 0; i <= 35; i++) {
-    let spans = document.createElement('span');
-    rightDiv.appendChild(spans);
+for (let i = 0; i <= 45; i++) {
+  let spans = document.createElement('span');
+
+  rightDiv.appendChild(spans);
 }
 
+let nextslide = () => {
+  let activeSlide = document.querySelector('.active');
+  activeSlide.classList.remove('active');
+  if (activeSlide.nextElementSibling) {
+    activeSlide.nextElementSibling.classList.add('active');
+    containerFirst.style.display='none'
+  };
+}; 
 
-
-
+discoverButton.addEventListener('click', ()=>{
+  nextslide();
+});
 
 
 
